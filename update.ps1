32ab61d34a314775bd881da0b7bc91fdc6d77277
Froxwin <56168224+Froxwin@users.noreply.github.com>
2021-09-27 10:52:18 +0530

    Updated Dependencies
    Updated old dependencies added new ones that were required and removed useless ones and added more dev tools

--- update.ps1
+++ update.ps1
@@ -1,4 +1,8 @@
+Write-Host ('=================|Repairing Files|================') -ForegroundColor Green
 npm i
+Write-Host ('==============|Updating Dependencies|=============') -ForegroundColor Green
 npm update
+Write-Host ('==============|Auditing Dependencies|=============') -ForegroundColor Green
 npm audit
+Write-Host ('=======|Dependencies for current workspace|=======') -ForegroundColor Cyan
 npm ls | lolcat
