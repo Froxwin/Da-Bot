ff2bcaa07e4126348d452ed3752c76a6128f618f
Froxwin <56168224+Froxwin@users.noreply.github.com>
2021-10-07 09:54:48 +0530

    hmm

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 require('dotenv').config()
 const fs = require('fs')
 const client = require('./config/client')
-
 const commandFolders = fs.readdirSync('.\\commands')
 for (const folder of commandFolders) {
   const commandFiles = fs.readdirSync(`.\\commands\\${folder}`)
-    .filter((f) => f.endsWith('.js'))
+    .filter((file) => file.endsWith('.js'))
   for (const file of commandFiles) {
     const command = require(`.\\commands\\${folder}\\${file}`)
     client.commands.set(command.name, command)
@@ -14,14 +13,14 @@ for (const folder of commandFolders) {
 const buttonFolders = fs.readdirSync('.\\modules')
 for (const folder of buttonFolders) {
   const buttonFiles = fs.readdirSync(`.\\modules\\${folder}`)
-    .filter((f) => f.endsWith('.js'))
+    .filter((file) => file.endsWith('.js'))
   for (const file of buttonFiles) {
     const button = require(`.\\modules\\${folder}\\${file}`)
     client.buttons.set(button.name, button)
   }
 }
 const events = fs.readdirSync('.\\events')
-  .filter((f) => f.endsWith('.js'))
+  .filter((file) => file.endsWith('.js'))
 for (const file of events) {
   /**
    * @type {import('./Classes/event')} event
@@ -33,5 +32,4 @@ for (const file of events) {
     client.on(event.name, (...args) => event.execute(...args))
   }
 }
-
 client.login(process.env.SOFTundWET)
