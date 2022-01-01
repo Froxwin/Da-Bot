32e044147433df00153ee4e01b5251487f1441d5
Froxwin <56168224+Froxwin@users.noreply.github.com>
2022-01-01 20:44:10 +0530

    snips
    snippits go brr

--- update.ps1
+++ update.ps1
@@ -1,14 +1,13 @@
-#!/usr/bin/env pwsh
 #Requires -Version 5
 
 [CmdletBinding()]
-param([bool][alias('l')]$lolcat)
+param([switch][alias('l')]$lolcat)
 
-Write-Host ('=================|Repairing Files|================') -ForegroundColor Green
-npm i
-Write-Host ('==============|Updating Dependencies|=============') -ForegroundColor Green
-npm update
-Write-Host ('==============|Auditing Dependencies|=============') -ForegroundColor Green
-npm audit
-Write-Host ('=======|Dependencies for current workspace|=======') -ForegroundColor Cyan
+Write-Host ('|||Repairing Files||>') -ForegroundColor Green
+npm.ps1 install
+Write-Host ('|||Updating Dependencies||>') -ForegroundColor Green
+npm.ps1 update
+Write-Host ('|||Auditing Dependencies||>') -ForegroundColor Green
+npm.ps1 audit
+Write-Host ('|||Dependencies for current workspace||>') -ForegroundColor Cyan
 $lolcat ? (npm ls | lolcat) : (npm ls)
