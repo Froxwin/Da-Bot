00118eeed5d19015ce4e78f4f66362f52107e453
Froxwin <56168224+Froxwin@users.noreply.github.com>
2022-01-22 22:02:40 +0530

    hmm

---
+++ scripts/cmdfetch.ps1
@@ -0,0 +1,12 @@
+#Requires -Version 7
+
+Get-ChildItem ('../src/commands') |
+  ForEach-Object {
+    Write-Host `
+      ("`e[1;38;2;226;165;255m[$($_.BaseName.ToUpper())]")
+    Get-ChildItem ($_) |
+      ForEach-Object {
+        Write-Host `
+          ("  `e[38;2;121;192;255m$($_.BaseName.ToUpper())")
+      }
+  }
--- scripts/update.ps1
+++ scripts/update.ps1
@@ -1,13 +1,16 @@
-#Requires -Version 7
-
+#Requires -Version 5
 [CmdletBinding()]
 param([switch][alias('l')]$lolcat)
 
-Write-Host ('|||Repairing Files||>') -ForegroundColor Green
+Write-Host('[Repairing Files]|>') `
+    -ForegroundColor Green
 npm install
-Write-Host ('|||Updating Dependencies||>') -ForegroundColor Green
+Write-Host('[Updating Dependencies]|>') `
+    -ForegroundColor Green
 npm update
-Write-Host ('|||Auditing Dependencies||>') -ForegroundColor Green
+Write-Host('[Auditing Dependencies]|>') `
+    -ForegroundColor Green
 npm audit
-Write-Host ('|||Dependencies for current workspace||>') -ForegroundColor Cyan
+Write-Host('[Workspace Dependencies]|>') `
+    -ForegroundColor Cyan
 $lolcat ? (npm ls | lolcat) : (npm ls)
