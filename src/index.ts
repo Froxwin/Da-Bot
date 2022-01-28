5114d1aac3393e0e4b002d108319ad572898010d
Froxwin <56168224+Froxwin@users.noreply.github.com>
2022-01-28 12:00:24 +0530

    b

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import ඞ = require('./engine/base/config/client');
 ඞ.load('commands', ඞ.commands)
 ඞ.load('modules', ඞ.buttons)
-require('fs').readdirSync('./engine/events')
+require('fs').readdirSync('./engine/base/events')
   .forEach(Φ => {
-    const δ = require(`./engine/events/${Φ}`)
+    const δ = require(`./engine/base/events/${Φ}`)
     ඞ[δ.once ? 'once' : 'on'](
       δ.name, (...Σ) => δ.execute(...Σ)
     )
   })
-require('./engine/config/token')('../.env')
+require('./engine/base/config/token')('../.env')
 ඞ.login(process.env.SOFTundWET)
+console.log(`PID: \x1b[93m${process.pid}\x1b[0m`)
+console.log(`NODE: \x1b[93m${process.version}\x1b[0m`)
